80c901a77bd0ebccb8113b03b37cb8b913673d5f
Tomi5 <tomasz.rysak@o2.pl>
2020-07-29 14:28:11 +0200

    add useEffect to MapContainer component

--- src/Components/MapContainer/MapContainer.tsx
+++ src/Components/MapContainer/MapContainer.tsx
@@ -1,29 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { ReactComponent as Map } from "../../assets/world.svg";
 import ColorPicker from "../ColorPicker/ColorPicker";
 import styles from "./MapContainer.module.scss";
-
-
 
 const MapContainer = () => {
   const [country, setCountry] = useState<string[]>([]);
   const [countryOnHover, setCountryOnHover] = useState("");
   const [pickedColor, setPickedColor] = useState("#428C08");
   const [tooltipPosition, setTooltipPosition] = useState({ x: null, y: null });
-console.log(tooltipPosition);
 
-
-
+  useEffect(() => {}, [countryOnHover, country, pickedColor]);
 
   const handleMouseMove = (e: any) => {
     const name = e.target.dataset.name;
     name ? setCountryOnHover(name) : setCountryOnHover("");
-    setTooltipPosition({ x: e.clientX + 15 , y: e.clientY + 15});
+    setTooltipPosition({ x: e.clientX + 15, y: e.clientY + 15 });
   };
 
   const handleClickOnMap = (e: { target: any }) => {
     const id: string = e.target.dataset.id;
-    id && (e.target.style.fill = pickedColor) && setCountry([...country, id]);
+    id && (e.target.style.fill = pickedColor);
+    id && setCountry([...country, id]);
   };
 
   const handleColorPicker = (e: { target: any }) => {
@@ -37,9 +34,16 @@ console.log(tooltipPosition);
         pickedColor={pickedColor}
         handleColorPicker={handleColorPicker}
       />
-      <div 
-      style={{position: "absolute", left: `${tooltipPosition.x}px`, top: `${tooltipPosition.y}px`}}
-      className={styles.tooltip}>{countryOnHover}</div>
+      <div
+        style={{
+          position: "absolute",
+          left: `${tooltipPosition.x}px`,
+          top: `${tooltipPosition.y}px`,
+        }}
+        className={styles.tooltip}
+      >
+        {countryOnHover}
+      </div>
     </div>
   );
 };
