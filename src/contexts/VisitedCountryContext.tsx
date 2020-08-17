bf60f1c4e94dc225725293645b01d71ace561e15
Tomi5 <tomasz.rysak@o2.pl>
2020-08-17 16:10:13 +0200

    create  VisitedCountryContext file

---
+++ src/contexts/VisitedCountryContext.tsx
@@ -0,0 +1,42 @@
+import React, { createContext, useState, ReactNode } from "react";
+
+interface IProps {
+  children: ReactNode;
+}
+
+type handleAddToVisited = {
+  (countryCode: string): void;
+};
+
+interface IContextProps {
+  visitedCountries: string[];
+  handleAddToVisited: handleAddToVisited;
+}
+
+export const VisitedCountryContext = createContext<IContextProps>({
+  visitedCountries: [],
+  handleAddToVisited: (e) => null,
+});
+
+const VisitedCountryContextProvider = ({ children }: IProps) => {
+  console.log("  VisitedContextProvider");
+
+  const [visitedCountries, setVsitedCountries] = useState<string[]>([]);
+
+  const handleAddToVisited: handleAddToVisited = (countryCode) => {
+    const isVisited = visitedCountries.find(
+      (visited) => visited === countryCode
+    );
+    !isVisited && setVsitedCountries([...visitedCountries, countryCode]);
+  };
+
+  const value = { visitedCountries, handleAddToVisited };
+
+  return (
+    <VisitedCountryContext.Provider value={value}>
+      {children}
+    </VisitedCountryContext.Provider>
+  );
+};
+
+export default VisitedCountryContextProvider;
