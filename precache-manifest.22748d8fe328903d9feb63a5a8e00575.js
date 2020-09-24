c802fc6406d75d2dff41b41d177558c6c14cfe78
Tomi5 <tomasz.rysak@o2.pl>
2020-09-24 12:00:01 +0200

    Updates

---
+++ precache-manifest.22748d8fe328903d9feb63a5a8e00575.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c975f635e8432a2834783c57743b132d",
+    "url": "/visited-countries-map/index.html"
+  },
+  {
+    "revision": "15a550b38a54e67bde64",
+    "url": "/visited-countries-map/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "15a550b38a54e67bde64",
+    "url": "/visited-countries-map/static/js/2.ee9c6305.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/visited-countries-map/static/js/2.ee9c6305.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "849c9eb3f9a6802ad81d",
+    "url": "/visited-countries-map/static/js/main.d01a2457.chunk.js"
+  },
+  {
+    "revision": "2354b943ec9ee7ede6e1",
+    "url": "/visited-countries-map/static/js/runtime-main.8882f1d8.js"
+  },
+  {
+    "revision": "c8612c73ee16255430f5cc977940edfd",
+    "url": "/visited-countries-map/static/media/world.c8612c73.svg"
+  }
+]);
--- src/styled.d.ts
+++
@@ -1,7 +0,0 @@
-import {} from "styled-components";
-import { lightTheme } from "./theme/theme";
-
-declare module "styled-components" {
-  type Theme = typeof lightTheme;
-  export interface DefaultTheme extends Theme {}
-}
